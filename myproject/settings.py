47ef01b14dbcd708dbbc9bef8db8501152329193
ceciliawambui <ceciliawambui026@gmail.com>
2025-01-09 10:35:32 +0300

    securing my secret key

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-bwstfnf=9(u4=4rw3gp6-9y1zp2pt&=hz^fz6cpv1*r(rzty5*'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
